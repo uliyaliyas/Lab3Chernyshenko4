8a9d806a5ea3d5c8bf42f3b1bef7f6f51064adec
5090969 <5090969@LAPTOP-NB62C61V>
2023-11-09 14:38:36 +0200

    Добавьте файлы проекта.

---
+++ Lab3Chernyshenko4/Program.cs
@@ -0,0 +1,13 @@
+﻿double x = 2;
+double step = 0.2;
+double end = 4.1;
+
+Console.WriteLine("x\t| y");
+
+while (x <= end)
+{
+    double y = Math.Abs(Math.Log10(x)) - Math.Pow((x - 2), 2);
+    Console.WriteLine($"{x}\t| {y}");
+
+    x += step;
+}
